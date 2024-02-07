fadc72e5d668911467df75daaf559cc5d10afe93
Mebaselassie Kidane <mebakid74@gmail.com>
2024-02-07 11:26:59 +0100

    Add files via upload

--- src/pi_camera_run.py
+++ src/pi_camera_run.py
@@ -1,8 +1,48 @@
-from picamera import PiCamera
-from time import sleep
+#from picamera import PiCamera
+#from time import sleep
 
+#camera = PiCamera()
+
+#camera.start_preview()
+#sleep(500)
+#camera.stop_preview()
+
+# import the necessary packages
+from picamera.array import PiRGBArray # Generates a 3D RGB array
+from picamera import PiCamera # Provides a Python interface for the RPi Camera Module
+import time # Provides time-related functions
+import cv2 # OpenCV library
+ 
+# Initialize the camera
 camera = PiCamera()
-
-camera.start_preview()
-sleep(500)
-camera.stop_preview()
+ 
+# Set the camera resolution
+camera.resolution = (640, 480)
+ 
+# Set the number of frames per second
+camera.framerate = 32
+ 
+# Generates a 3D RGB array and stores it in rawCapture
+raw_capture = PiRGBArray(camera, size=(640, 480))
+ 
+# Wait a certain number of seconds to allow the camera time to warmup
+time.sleep(0.1)
+ 
+# Capture frames continuously from the camera
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+     
+    # Grab the raw NumPy array representing the image
+    image = frame.array
+     
+    # Display the frame using OpenCV
+    cv2.imshow("Frame", image)
+     
+    # Wait for keyPress for 1 millisecond
+    key = cv2.waitKey(1) & 0xFF
+     
+    # Clear the stream in preparation for the next frame
+    raw_capture.truncate(0)
+     
+    # If the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
