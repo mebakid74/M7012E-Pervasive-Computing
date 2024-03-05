d9c3efb74d0beac6cb4b48798070ff360c79e4de
berkanmen <147723528+berkanmen@users.noreply.github.com>
2024-03-05 23:21:53 +0100

    jdskajd

--- src/PTZ Camera/yoloversion.py
+++ src/PTZ Camera/yoloversion.py
@@ -42,6 +42,27 @@ net = cv2.dnn.readNet(args.weights, args.config)
 conf_threshold = 0.3
 nms_threshold = 0.4
 
+def control_camera(pan, tilt):
+    url = "http://130.240.105.144/cgi-bin/aw_ptz"
+    command = "#APC{}{}".format(pan, tilt)
+    params = {
+        "cmd": command,
+        "res": 1
+    }
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            print("Command sent successfully")
+            print("Response:", response.status_code)
+        else:
+            print("Failed to send command. Status code:", response.status_code)
+    except Exception as e:
+        print("An error occurred:", e)
+
+# Example: Sending the command to set pan to 7FFF and tilt to 7FFF
+#control_camera("86BB", "76BF")
+
+
 
 
 
